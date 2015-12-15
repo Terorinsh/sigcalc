3234cd5501d3319dec7f408f13827ec5c7984bb6
Karlis Kalejs <info@karliskalejs.id.lv>
2015-12-15 08:29:28 +0000

    Sigmanet logo lol

---
+++ sigmanet.py
@@ -0,0 +1,135 @@
+import time
+import os
+from random import randint
+t = 0
+x = 0
+y = None
+
+def rand():
+    x = (randint(1,4))
+#    x = x * ' '
+    return x
+
+#while True:
+#   t = t + 1
+#   time.sleep(0.1)
+#   os.system('cls' if os.name == 'nt' else 'clear')
+#   print rand(),"all",rand(),"work",rand(),"and",rand(),"no",rand(),"play",rand(),"makes",rand(),"jack",rand(),"a",rand(),"dull",rand(),"boy"                                                                                                                                                                                                                              
+    
+while True:
+    time.sleep(0.3)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if rand() == 2:
+        print " "
+    else:
+        print"                                             #######                            "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"                                           ###     ###                          "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"                                         ##           ##                        "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"                                        ##             ##                       "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"       ##                              ##               ##                   #  "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"       #                              ##                 ##                 ##  "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"                                            #########                       ##  "
+
+    if rand() == 2:
+        print " "
+    else:
+        print" ##### ##   ###### ###### ####   #####       ##           ## ####    ####  #####"
+
+    if rand() == 2:
+        print " "
+    else:
+        print"##   # ##  ##  ##  ##   ##   #  ##   #        ##           ##   #   #    #  ##  "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"##     ##  #   ##  ##   ##   #       #         ##          #    #  ##   ##  ##  "
+
+    if rand() == 2:
+        print " "
+    else:
+        print" ##    ##  ##  ##  ##   #    #      ##          ##         #    #  ##       ##  "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"  ###  ##  #####   ##   #    #    ## #           ##        #    #  ##       ##  "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"    ## ##   #      ##   #    #   #   #          ##         #    #  ##       ##  "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"    ## ##  #       ##   #    #  ##   #         ##          #    #  ##       ##  "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"#   ## ##  ####    ##   #    #  ##  ##        ##           #    #   ###  #  ##  "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"#####  ##   # ###  ##   ##   ##  ### ##      ##            ##   ##   ####    ###"
+
+    if rand() == 2:
+        print " "
+    else:
+        print"           #    ##                          #########                           "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"          #      #                    ##                 ##                     "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"         ##      #                     ##               ##                      "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"          ##    #                       ##             ##                       "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"          ######                         ##           ##                        "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"                                           ###     ###                          "
+
+    if rand() == 2:
+        print " "
+    else:
+        print"                                             #######                            "
